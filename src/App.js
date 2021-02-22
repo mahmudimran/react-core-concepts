c476ab4388a6a395b046ec0e82f1b439f9c57daa
mahmud imran <mahmudimran203@gmail.com>
2021-02-23 03:13:37 +0600

    Pass dynamic data to components, props in react

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     
   // }
 
+   const nayoks =['rajjak','almgir','elliskanchon','manna']
   return (
     <div className="App">
       <header className="App-header">
@@ -30,31 +31,43 @@ function App() {
         <h3 className="" style={style}>My Heading React : {person_gf.name +" "+ person_gf.age}</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, doloribus.</p> */}
 
-        <h1>Hey I am a React Person</h1>
-        <People></People>
-        <People></People>
-        <People></People>
-
+        {/* <h1>Hey I am a React Person</h1>
+        <People name={nayoks[0]} naika="mousumi"></People>
+        <People name={nayoks[1]}></People>
+        <People name={nayoks[2]}></People>
+        <People></People> */}
+        <People name="munna" job="webdev"></People>
+        <People name="jorina" job="fb"></People>
       </header>
     </div>
   );
 }
-function People(){
-  const peopleStyle={
-    borderRadius:'5px',
-    padding:'10px',
-    margin:'10px',
-    backgroundColor:'white',
-    color:'black'
-  }
-  return ( 
-  <div style={peopleStyle}>
-    <h1>name : Toktoki</h1>
-    <h3>Name : My name Is khan</h3>
-  </div>
+
+// props mane hosse property er shortverson
+// function People(props){
+//   const peopleStyle={
+//     borderRadius:'5px',
+//     padding:'10px',
+//     margin:'10px',
+//     backgroundColor:'white',
+//     color:'black'
+//   }
+//   console.log(props)
+//   return ( 
+//   <div style={peopleStyle}>
+//     <h1>naYok : {props.name} </h1>
+//     <h3>Naika : {props.naika}</h3>
+//   </div>
+//   )
+// }
+
+function People(props){
+  return (
+    <div style={{border:'2px solid gray',margin:'10px'}}>
+      <h3>My name :{props.name}</h3>
+      <p>My profeson :{props.job}</p>
+    </div>
   )
 }
-
-
 
 export default App;
